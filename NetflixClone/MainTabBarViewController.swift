8ce9eb45ca0f45d54c7b2969852de74a4cf19970
Zeynep Özdemir Açıkgöz <zeynmirgmail.com>
2022-12-05 15:46:16 +0300

    first commit

--- NetflixClone/MainTabBarViewController.swift
+++ NetflixClone/MainTabBarViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainTabBarViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ NetflixClone/Views/CollectionViewTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CollectionViewTableViewCell.swift
+//  NetflixClone
+//
+//  Created by Zeynep Özdemir Açıkgöz on 5.12.2022.
+//
+
+import UIKit
+
+class CollectionViewTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
