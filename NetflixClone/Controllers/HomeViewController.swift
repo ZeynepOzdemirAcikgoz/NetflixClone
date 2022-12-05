6d60d2dde7e906d000731e2259033d808eb4554e
Zeynep Özdemir Açıkgöz <zeynmirgmail.com>
2022-12-05 15:48:05 +0300

    first

--- NetflixClone/Controllers/HomeViewController.swift
+++ NetflixClone/Controllers/HomeViewController.swift
@@ -9,21 +9,55 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    
+    private var homeFeedTable: UITableView = {
+        
+        let table = UITableView(frame: .zero, style: .grouped)
+        table.register(CollectionViewTableViewCell.self, forCellReuseIdentifier: CollectionViewTableViewCell.identifier)
+        return table
+        
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+        view.addSubview(homeFeedTable)
+        
+        homeFeedTable.delegate = self
+        homeFeedTable.dataSource = self
 
-        // Do any additional setup after loading the view.
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        homeFeedTable.frame = view.bounds
+    }
+
+
+   
+}
+
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
     }
-    */
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CollectionViewTableViewCell.identifier, for: indexPath) as? CollectionViewTableViewCell else{
+            return UITableViewCell()
+        }
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 40
+    }
 }
--- NetflixClone/Views/CollectionViewTableViewCell.swift
+++ NetflixClone/Views/CollectionViewTableViewCell.swift
@@ -9,15 +9,17 @@ import UIKit
 
 class CollectionViewTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+  static let identifier = "CollectionViewTableViewCell"
+    
+    override init (style: UITableViewCell.CellStyle, reuseIdentifier: String?){
+        
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.backgroundColor = .systemMint
+        
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder: NSCoder) {
+        fatalError()
     }
 
 }
